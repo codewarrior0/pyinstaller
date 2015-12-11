c8f435b9cb336d3ea1f3a36f79a3157c9377ab58
David Vierra <codewarrior@hawaii.rr.com>
2015-12-11 02:23:13 -1000

    Issue #1431: Create FATAL_PERROR and FATAL_WINERROR macros
    # Conflicts:
    #	bootloader/src/pyi_global.c
    #	bootloader/src/pyi_global.h
    #	bootloader/src/pyi_win32_utils.c

--- bootloader/src/pyi_global.h
+++ bootloader/src/pyi_global.h
@@ -78,6 +78,8 @@ typedef int bool;
  */
 
 void pyi_global_printf(const char *fmt, ...);
+void pyi_global_perror(const char *funcname, const char *fmt, ...);
+void pyi_global_winerror(const char *funcname, const char *fmt, ...);
 
 /*
  * On Windows and with windowed mode (no console) show error messages
@@ -90,10 +92,19 @@ void mbfatalerror(const char *fmt, ...);
 
 void mbothererror(const char *fmt, ...);
     #define OTHERERROR mbothererror
+
+    void mbfatal_perror(const char *funcname, const char *fmt, ...);
+    #define FATAL_PERROR mbfatal_perror
+
+    void mbfatal_winerror(const char *funcname, const char *fmt, ...);
+    #define FATAL_WINERROR mbfatal_winerror
+
 #else
 /* TODO copy over stbprint to bootloader. */
     #define FATALERROR pyi_global_printf
     #define OTHERERROR pyi_global_printf
+    #define FATAL_PERROR pyi_global_perror
+    #define FATAL_WINERROR pyi_global_winerror
 #endif  /* WIN32 and WINDOWED */
 
 /* Enable or disable debug output. */
